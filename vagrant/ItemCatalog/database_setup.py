ad5eefac4f307dc7ef447b59dd2133bdb376fa52
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-03-28 19:27:35 -0400

    Added the import for create_engine to the file database_setup.py.

--- vagrant/ItemCatalog/database_setup.py
+++ vagrant/ItemCatalog/database_setup.py
@@ -2,3 +2,4 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
